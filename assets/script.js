d249cd1b37d7f128dc209ce874f735e7625d93db
Pablo de Paula <pablo270404@gmail.com>
2022-11-28 19:42:27 -0300

    website finished

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,19 @@ for (let question of questionBox) {
     question.classList.toggle("question-active");
   });
 }
+
+window.addEventListener("scroll", reveal);
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var revealTop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 150;
+
+    if (revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add("active");
+    }
+  }
+}
